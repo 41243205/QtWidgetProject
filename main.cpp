7e26d6eb75e2c9d3088aaa0957f7f70a4ca47c48
FREE <84310382+FreeSob@users.noreply.github.com>
2024-10-07 19:46:26 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -18,10 +18,28 @@ int main(int argc, char *argv[]) {
     leaderLayout->addWidget(leaderLabel);
     leaderTab->setLayout(leaderLayout);
 
+    
+    QWidget *member3Tab = new QWidget;
+    QLabel *member3Label = new QLabel("這是組員3頁面");
+    QPushButton *fileButton = new QPushButton("File select");
+    QVBoxLayout *member3Layout = new QVBoxLayout;
+    member3Layout->addWidget(member3Label);
+    member3Layout->addWidget(fileButton);
+    member3Tab->setLayout(member3Layout);
+
+
+    QObject::connect(fileButton, &QPushButton::clicked, [=]() {
+        QString filePath = QFileDialog::getOpenFileName(nullptr, "選擇檔案", "", "所有檔案 (*.*)");
+        if (!filePath.isEmpty()) {
+            leaderLabel->setText(filePath);
+        }
+    });
+
+
     tabWidget->addTab(leaderTab, "隊長");
-    //tabWidget->addTab(member1Tab, "組員1");
-    //tabWidget->addTab(member2Tab, "組員2");
-    //tabWidget->addTab(member3Tab, "組員3");
+    tabWidget->addTab(member1Tab, "組員1");
+    tabWidget->addTab(member2Tab, "組員2");
+    tabWidget->addTab(member3Tab, "組員3");
 
     tabWidget->setWindowTitle("example");
     tabWidget->resize(400, 300);
