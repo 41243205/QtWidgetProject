0208e95fd8a654e8e05ac8d77ea406b2321e46e2
FREE <84310382+FreeSob@users.noreply.github.com>
2024-10-07 22:01:09 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-
 #include <QApplication>
 #include <QTabWidget>
 #include <QLabel>
@@ -19,7 +18,8 @@ int main(int argc, char *argv[]) {
     leaderLayout->addWidget(leaderLabel);
     leaderTab->setLayout(leaderLayout);
 
-    
+   
+
     QWidget *member3Tab = new QWidget;
     QLabel *member3Label = new QLabel("這是組員3頁面");
     QPushButton *fileButton = new QPushButton("File select");
@@ -48,69 +48,3 @@ int main(int argc, char *argv[]) {
 
     return app.exec();
 }
-
-#include <QApplication>
-#include <QTabWidget>
-#include <QLabel>
-#include <QWidget>
-#include <QVBoxLayout>
-#include <QPushButton>
-#include <QFileDialog>
-#include <QColorDialog>
-#include <QFontDialog>
-
-int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
-    QTabWidget *tabWidget = new QTabWidget;
-    QWidget *leaderTab = new QWidget;
-    QLabel *leaderLabel = new QLabel("隊長:張志華 41243227\n組員1 徐聖硯 41243128\n組員2 黃詩方 41243205\n組員3 陳霑汶 50915110");
-    leaderLabel->setStyleSheet("color: blue;");
-    QVBoxLayout *leaderLayout = new QVBoxLayout;
-    leaderLayout->addWidget(leaderLabel);
-    leaderTab->setLayout(leaderLayout);
-        QWidget *member1Tab = new QWidget;
-    QLabel *member1Label = new QLabel("這是組員1頁面");
-    QPushButton *colorButton = new QPushButton("color select");
-    QVBoxLayout *member1Layout = new QVBoxLayout;
-    member1Layout->addWidget(member1Label);
-    member1Layout->addWidget(colorButton);
-    member1Tab->setLayout(member1Layout);
-
-
-    QObject::connect(colorButton, &QPushButton::clicked, [=]() {
-        QColor color = QColorDialog::getColor(Qt::white, nullptr, "選擇文字顏色");
-        if (color.isValid()) {
-            QString colorName = color.name();
-            leaderLabel->setStyleSheet(QString("color: %1;").arg(colorName));
-        }
-    });
-
-    QWidget *member2Tab = new QWidget;
-    QLabel *member2Label = new QLabel("這是組員2頁面");
-    QPushButton *styleButton = new QPushButton("Front select");
-    QVBoxLayout *member2Layout = new QVBoxLayout;
-    member2Layout->addWidget(member2Label);
-    member2Layout->addWidget(styleButton);
-    member2Tab->setLayout(member2Layout);
-
-
-    QObject::connect(styleButton, &QPushButton::clicked, [=]() {
-        bool ok;
-        QFont font = QFontDialog::getFont(&ok, leaderLabel->font(), nullptr, "選擇字體");
-        if (ok) {
-            leaderLabel->setFont(font);
-        }
-    });
-
-    tabWidget->addTab(leaderTab, "隊長");
-    tabWidget->addTab(member1Tab, "組員1");
-    tabWidget->addTab(member2Tab, "組員2");
-    //tabWidget->addTab(member3Tab, "組員3");
-
-    tabWidget->setWindowTitle("example");
-    tabWidget->resize(400, 300);
-    tabWidget->show();
-
-    return app.exec();
-}
-
